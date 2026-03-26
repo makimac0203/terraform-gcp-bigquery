264eb01b982e82f5ea7960c1af5879616a391712
makimac0203 <makimac0203@gmail.com>
2026-03-26 11:39:08 +0900

    fix apis.tf

--- modules/apis.tf
+++ modules/apis.tf
@@ -4,18 +4,18 @@ variable "project_id" {
 }
 
 variable "apis" {
-  description = "有効化するAPIのリスト"
-  type        = list(string)
-  default = [
-    "bigquery.googleapis.com",
-  ]
+  description = "有効化するAPIのマップ。キー=API名, 値=説明"
+  type        = map(string)
+  default = {
+    "bigquery.googleapis.com" = "BigQuery API"
+  }
 }
 
 resource "google_project_service" "apis" {
-  for_each = toset(var.apis)
+  for_each = var.apis
 
   project                    = var.project_id
-  service                    = each.value
+  service                    = each.key
   disable_dependent_services = false
   disable_on_destroy         = false
 }
